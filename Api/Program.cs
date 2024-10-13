52e9b37acacb5f0dd01c61a9920e3f6c9e7895bc
jvelazquez <jonathanvgms@gmail.com>
2024-10-13 13:44:26 -0300

    Initial commit

---
+++ Api/Program.cs
@@ -0,0 +1,21 @@
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger(options =>
+    {
+        options.RouteTemplate = "openapi/{documentName}.json";
+    });
+    app.MapScalarApiReference();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
